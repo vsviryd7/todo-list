23afd66092d025df7a2b7afa09d509f7cded7483
vsviryd7 <ol.sviridenko777@gmail.com>
2026-04-27 16:41:07 -0400

    formatiing and add prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 import './App.css'
-import TodoList from './TodoList.jsx';
-import TodoForm from './TodoForm.jsx';
+import TodoList from './TodoList.jsx'
+import TodoForm from './TodoForm.jsx'
 
 function App() {
-
-  return (
-    <div>
-      <h1>My Todos</h1>
-        <TodoForm />
-        <TodoList />
-    </div>
-  );
+    return (
+        <div>
+            <h1>My Todos</h1>
+            <TodoForm />
+            <TodoList />
+        </div>
+    )
 }
 
-export default App;
+export default App
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,19 +1,18 @@
-
-
 function TodoList() {
-	const todoList = [
-    { id: 1, title: "review resources" },
-    { id: 2, title: "take notes" },
-    { id: 3, title: "code out app" },
-  ];
+    const qwerty = 1234
+    const todoList = [
+        { id: 1, title: 'review resources' },
+        { id: 2, title: 'take notes' },
+        { id: 3, title: 'code out app' },
+    ]
 
-  return (
-    <ul>
-      {todoList.map(todo => (
-        <li key={todo.id}>{todo.title}</li>
-      ))}
-    </ul>
-  );
+    return (
+        <ul>
+            {todoList.map((todo) => (
+                <li key={todo.id}>{todo.title}</li>
+            ))}
+        </ul>
+    )
 }
 
-export default TodoList;
+export default TodoList
